df63f76090926684fc201b3f92e7b986200df300
Chris Gonzalez <christogonz92@gmail.com>
2024-07-31 16:13:23 +0200

    [Feature] Add Parallax ScrollView.

--- ParallaxScrollView/ContentView.swift
+++ ParallaxScrollView/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let imageName = ["building 1", "building 2", "building 3", "building 4", "building 5", "building 6", "building 7",]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 16) {
+                ForEach(imageName, id: \.self) { imageName in
+                    VStack {
+                        ZStack {
+                            Image(imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .scrollTransition(axis: .horizontal) { content, phase in
+                                    content
+                                        .offset(x: phase.isIdentity ? 0 : phase.value * -200)
+                                }
+                        }
+                        .containerRelativeFrame(.horizontal)
+                        .clipShape(RoundedRectangle(cornerRadius: 36))
+                        .shadow(color: .gray.opacity(0.20), radius: 25, x: 5, y: 10)
+                        .scrollTransition(axis: .horizontal) { content, phase in
+                            content
+                                .scaleEffect(phase.isIdentity ? 0.95 : 0.50)
+                        }
+                        
+                        Text(imageName)
+                            .scrollTransition(axis: .horizontal) { content, phase in
+                                content
+                                    .opacity(phase.isIdentity ? 1 : 0.85)
+                                    .offset(x: phase.value * 200)
+                            }
+                    }
+                    
+                }
+            }
         }
-        .padding()
+        .contentMargins(36)
+        .scrollTargetBehavior(.paging)
+        .scrollIndicators(.hidden)
     }
 }
 
