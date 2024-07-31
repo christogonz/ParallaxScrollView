be31f2ede0bfcd802d273bf3fe8d727c3cabf939
Chris Gonzalez <christogonz92@gmail.com>
2024-07-31 15:29:13 +0200

    Initial Commit

---
+++ ParallaxScrollView/ParallaxScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxScrollViewApp.swift
+//  ParallaxScrollView
+//
+//  Created by Christopher Gonzalez on 2024-07-31.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
